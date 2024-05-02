894030ece41afe7b2b6b52e1ec9784ae05eb5441
imran61999 <smemrannit@gmail.com>
2024-05-02 16:13:11 +0600

    added banner section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Bannar from "../Bannar/Bannar";
 
 
 const Home = () => {
     return (
         <div>
+            <Bannar></Bannar>
             <h2>This is home</h2>
         </div>
     );
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <li><Link to="/about">About</Link></li>
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 h-28 mb-4">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
